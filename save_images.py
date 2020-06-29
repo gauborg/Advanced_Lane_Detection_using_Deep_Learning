1dee0a63fcc2e7abc4d42cf522d687ac25d154b6
gauborg <gau.borg@gmail.com>
2020-06-29 09:15:57 -0400

    modified code to work on multiple video files

--- save_images.py
+++ save_images.py
@@ -1,46 +1,70 @@
 # code for extracting images from a video stream
 
 '''
-Description: This code extracts images from a given video stream.
+Description: This code extracts images from a given video files ...
 Author: Gaurav Borgaonkar
 Date: 15 June 2020
+Update Date: 23 June, 2020
 
 '''
 
 import os
 import cv2
- 
-# Opens the Video file
-filename = "../data_for_lane_detection/videos/video1.mp4"
+import glob
+import tqdm
 
+file_list = glob.glob("../data_for_lane_detection/videos/video*")
+total_videos = len(file_list)
+
+# image index number for a million range
+i = 1000001
+
+# parameter to count the number of frames which could not be extracted
+error_frame = 0
+
+print("Started the image extraction ...")
 # create a directory with the same name and '-images' for saving images
 try:
-    os.mkdir("images")
+    os.mkdir("../data_for_lane_detection/images")
     print("Created a new folder named images ...")
 except FileExistsError:
     print("Folder with name images already exists!!!")
     print("Images will be merged with the contents of the existing folder!")
     pass
 
-# code for extracting frames starts here ...
-video = cv2.VideoCapture(filename)
+for item in file_list:
 
-# image number sequence to start with
-i = 1000001
-
-while(video.isOpened()):
+    print("Working on file " + item)
+    # Opens the Video file
+    # code for extracting frames starts here ...
+    video = cv2.VideoCapture(item)
+    # time.sleep(0.01)
+    while(video.isOpened()):
+        # extract frame from the video file
+        ret, frame = video.read()
+        if(ret == True):
+            frame = cv2.resize(frame, None, fx = 0.75, fy = 0.75, interpolation = cv2.INTER_CUBIC)
+            # path to write images to ...
+            cv2.imwrite('../data_for_lane_detection/images/' + 'image' + str(i) + '.jpg', frame)
+            # increase image sequence number
+            i += 1
+        else:
+            error_frame += 1
+            break
     
-    ret, frame = video.read()
-    if(ret == True):
-        frame = cv2.resize(frame, None, fx = 1.0, fy = 1.0, interpolation = cv2.INTER_CUBIC)
-        # path to write images to ...
-        cv2.imwrite('../data_for_lane_detection/images/' + 'image' + str(i) + '.jpg', frame)
-        i += 1
-    else:
-        break
+    video.release()
+    cv2.destroyAllWindows()
+
+    print(f"Extraction complete for file {item} ...")
+    print()
+    # end of for loop
 
 
-video.release()
+# print the summary
+images = glob.glob("../data_for_lane_detection/images/image*")
+num_images = (len(images))
 
-cv2.destroyAllWindows()
+print(f"{num_images} images extracted from {total_videos} video files ...")
+
+
 
